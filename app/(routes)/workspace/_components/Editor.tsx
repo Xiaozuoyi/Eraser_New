0bb8ebe200afa18689d974fc15a97f2f37322bd5
pengdan <pengdangpeng@gmail.com>
2024-03-13 23:52:06 +0800

    Add header styles and dependencies

--- app/(routes)/workspace/_components/Editor.tsx
+++ app/(routes)/workspace/_components/Editor.tsx
@@ -1,15 +1,19 @@
 'use client';
 import { useEffect, useRef } from 'react';
 import EditorJS from '@editorjs/editorjs';
+import Header from '@editorjs/header';
 // 该函数将确保组件只渲染一次
-const RenderEditor = (ElementId: string) => {
+const RenderEditor = (ElementId: string = 'editorjs') => {
   const rendered = useRef<false | true>(false);
   useEffect(() => {
     if (!rendered.current) {
       rendered.current = true;
       //在这里，我们调用 EditorJS 运行，还可以向它传递运行所需的参数
       new EditorJS({
-        holder: ElementId
+        holder: ElementId,
+        tools: {
+          header: Header
+        }
       });
     } else {
       return;
@@ -20,7 +24,7 @@ const RenderEditor = (ElementId: string) => {
 function Editor() {
   const editorId = 'editorjs';
   RenderEditor(editorId);
-  return <div id={editorId}></div>;
+  return <div id={editorId} className="ml-20"></div>;
 }
 
 export default Editor;
